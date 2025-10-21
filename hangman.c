d989e65d96db75d68dd120d56c414d55aaee50f1
TheJellyfishWitch <addie.erickson@ucas-edu.net>
2025-10-21 14:41:52 -0600

    not done

---
+++ hangman.c
@@ -0,0 +1,21 @@
+// AE, JL, EG, XP, EM 7th Hangman Video Game
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+//Xander P, general user introduction and greeting
+
+//Eva Gilbert, making the board
+void hangman(wrong){
+    if (int wrong)
+}
+
+// Jovie L. word list
+
+int main(void){ 
+    char words[][15] = {"pancakes", "waffles","burgers", "cinnamonrolls", "scooter","cake","pizza","supercalifragilisticexpialidocious","jumpsuit","pneumonoultramicroscopicsilicovolcanoconiosis","minions","microsopic","water","apples","radioactive","pasta","tortellini","jellyfish","fruitsnacks","red","stingray","octopus","smiley face","","bike","pinapples","GERALD","cherries","harold","supernova","hangman",};
+
+    return 0;
+} 
+
